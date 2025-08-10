48bbb0e973acea7c327020e1b28160288bdcc3ee
Prem K <premkoyilada222@gmail.com>
2025-08-10 22:16:07 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Prem Koyilada",
+    author_email="premkoyilada222@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are a medical assistant for question anwering tasks"
+    "Use the following pieces of retrieved context to answer"
+    "The question. if you dont know the answer, say that you don't know"
+    "Use three sentences maximum and keep the answers concise."
+    "\n\n"
+    "{context}"
+)
